aabf731f13299736e698ab7e11b842ed1554920a
Alexander Tkachenko <xander.tka4enko@gmail.com>
2025-07-16 22:14:05 +0700

    Исправил extension-метод Pool.Add() на возвращение компонента

--- Surtility/Extensions/PoolExtensions.cs
+++ Surtility/Extensions/PoolExtensions.cs
@@ -4,9 +4,11 @@ namespace Surtility.Extensions;
 
 public static class PoolExtensions
 {
-    public static void Add<T>(this EcsPool<T> pool, int entity, T componentValue) where T : struct
+    public static ref T Add<T>(this EcsPool<T> pool, int entity, T componentValue) where T : struct
     {
         ref var addedComponent = ref pool.Add(entity);
         addedComponent = componentValue;
+
+        return ref addedComponent;
     }
 }
