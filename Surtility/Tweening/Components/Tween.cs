9e40a70497420b9efb3e211cfd4bc9abe2a8ca22
Alexander Tkachenko <xander.tka4enko@gmail.com>
2025-07-17 14:36:50 +0700

    Добавил дополнительный конструктор для Tween

--- Surtility/Tweening/Components/Tween.cs
+++ Surtility/Tweening/Components/Tween.cs
@@ -2,6 +2,12 @@ namespace Surtility.Tweening.Components;
 
 public struct Tween(double duration)
 {
+    public Tween(double duration, float percent) : this(duration)
+    {
+        Percent = percent;
+        CurrentTime = duration * percent;
+    }
+
     public double Duration = duration;
     public double CurrentTime;
     public float Percent;
